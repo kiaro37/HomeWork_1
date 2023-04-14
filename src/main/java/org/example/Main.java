4a3bd381c65d2f43f93b8179e287f1fe6763dd62
kiaro37 <iva.sakharov.e@gmail.com>
2023-04-14 15:21:54 +0300

    Task 1 complete

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,23 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        //ex1();
-        ex2();
+        ex1();
+        //ex2();
     }
 
     private static void ex1() {
         //1. Написать метод, принимающий на вход два аргумента: len и initialValue, и возвращающий одномерный
         //массив типа int длиной len, каждая ячейка которого равна initialValue;
-
+        int len = 5;
+        int initialValue = 3;
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = initialValue;
+        }
+        System.out.println(Arrays.toString(arr));
     }
 
     private static void ex2() {
@@ -17,13 +25,13 @@ public class Main {
         int[] arr = new int[]{2, 4, 1, 0, 5, 7, 9, 12};
         int maxValue = arr[0];
         int minValue = arr[0];
-        for (int i = 0; i <= arr.length -1; i++
+        for (int i = 0; i <= arr.length - 1; i++
         ) {
             if (arr[i] > maxValue) {
                 maxValue = arr[i];
             }
         }
-        for (int i = 0; i <= arr.length -1; i++
+        for (int i = 0; i <= arr.length - 1; i++
         ) {
             if (arr[i] < minValue) {
                 minValue = arr[i];
